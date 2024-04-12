3a5b9182d4097455769d8c41d5d38d9be8ee452d
cla-gambino <154328607+cla-gambino@users.noreply.github.com>
2024-04-12 17:21:26 +0200

    Initial commit

---
+++ src/main/java/develhope/esercizio77testiamolabasedati/Esercizio77TestiamoLaBaseDatiApplication.java
@@ -0,0 +1,13 @@
+package develhope.esercizio77testiamolabasedati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercizio77TestiamoLaBaseDatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esercizio77TestiamoLaBaseDatiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/develhope/esercizio77testiamolabasedati/entities/Students.java
@@ -0,0 +1,27 @@
+package develhope.esercizio77testiamolabasedati.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Students {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(unique = true, name = "userEmail", length = 100, nullable = false)
+    private String email;
+}
